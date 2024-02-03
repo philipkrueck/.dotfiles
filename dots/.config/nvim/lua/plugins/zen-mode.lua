c09046c270ddfeda10a651a427057b673add772d
Philip Krück <hi@philipkrueck.com>
2024-02-03 20:21:47 +0700

    add zen mode plugin for NeoVim

---
+++ dots/.config/nvim/lua/plugins/zen-mode.lua
@@ -0,0 +1,31 @@
+return {
+  "folke/zen-mode.nvim",
+  opts = {
+    window = {
+      backdrop = 0.95,
+      width = 90, -- width of the Zen window (default: 120)
+      height = 1, -- height of the Zen window
+      options = {
+        signcolumn = "no", -- disable signcolumn
+        number = false, -- disable number column
+        relativenumber = true, -- keep relative numbers
+      },
+    },
+    plugins = {
+      -- disable some global vim options (vim.o...)
+      options = {
+        enabled = true,
+        ruler = false, -- disables the ruler text in the cmd line area
+        showcmd = false, -- disables the command in the last line of the screen
+        laststatus = 0, -- turn off the statusline in zen mode
+      },
+      twilight = { enabled = true }, -- enable to start Twilight when zen mode opens
+      gitsigns = { enabled = false }, -- disables git signs
+      tmux = { enabled = true }, -- disables the tmux statusline
+      wezterm = {
+        enabled = true,
+        font = "+20", -- (10% increase per step)
+      },
+    },
+  },
+}
