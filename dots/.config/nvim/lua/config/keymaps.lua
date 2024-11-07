c6991ce5e2587d933156e5c0020621b4949b31a0
Philip Krück <hi@philipkrueck.com>
2024-11-07 12:33:16 +0700

    add tmux-sessionizer within neovim

--- dots/.config/nvim/lua/config/keymaps.lua
+++ dots/.config/nvim/lua/config/keymaps.lua
@@ -3,4 +3,11 @@
 -- Add any additional keymaps here
 
 vim.keymap.set("n", "<C-l>", "<C-w>l", { desc = "Go to right window", remap = true })
-vim.keymap.set("n", "<C-f>", ":!tmux neww tmux-sessionizer<CR>", { noremap = true, silent = true })
+
+vim.keymap.set(
+  "n",
+  "<C-f>",
+  ":!tmux neww nu ~/.config/nushell/tmux-sessionizer.nu<CR>",
+  { noremap = true, silent = true }
+)
+-- vim.keymap.set("n", "<C-f>", ":!tmux neww tmux-sessionizer<CR>", { noremap = true, silent = true })
