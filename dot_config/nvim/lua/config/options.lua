260fb9fbccf3332d45abb2ce020fceb0f1a4fe28
Philip Krück <hi@philipkrueck.com>
2025-09-06 10:20:06 +0000

    feat: update global neovim options

--- dot_config/nvim/lua/config/options.lua
+++ dot_config/nvim/lua/config/options.lua
@@ -7,6 +7,12 @@ vim.g.lazyvim_check_order = false
 -- ignore case sensitivity when searching
 vim.opt.ignorecase = true
 
+-- show line numbers
+vim.opt.number = true
+
+-- show relative line numbers
+vim.opt.relativenumber = true
+
 -- minimal number of screen lines above/below curser when scrolling;
 -- large number keeps it centered
 vim.opt.scrolloff = 999
