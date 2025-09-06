1aeb7aa037e9f78d78532f8720a7fc39edf027bd
Philip Krück <hi@philipkrueck.com>
2025-09-06 10:14:35 +0000

    feat: disable bufferline

--- dot_config/nvim/lua/plugins/disabled.lua
+++ dot_config/nvim/lua/plugins/disabled.lua
@@ -1,4 +1,5 @@
 return {
-  { "rcarriga/nvim-notify", enabled = false },
-  { "folke/noice.nvim", enabled = false },
+	{ "rcarriga/nvim-notify", enabled = false },
+	{ "folke/noice.nvim", enabled = false },
+	{ "akinsho/bufferline.nvim", enabled = false },
 }
