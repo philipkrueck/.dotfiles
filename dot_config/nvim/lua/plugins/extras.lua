cae6d1a8c2ef5419703b4bd1d5528d4aab6a19eb
Philip Krück <hi@philipkrueck.com>
2025-09-25 16:58:46 +0100

    fix: lazy extras formatting

--- dot_config/nvim/lua/plugins/extras.lua
+++ dot_config/nvim/lua/plugins/extras.lua
@@ -1,18 +1,18 @@
 return {
-  -- languages
+	-- languages
 	{ import = "lazyvim.plugins.extras.lang.json" },
 	{ import = "lazyvim.plugins.extras.lang.toml" },
 	{ import = "lazyvim.plugins.extras.lang.yaml" },
-  { import = "lazyvim.plugins.extras.lang.go"}
-  { import = "lazyvim.plugins.extras.lang.python"}
-  { import = "lazyvim.plugins.extras.lang.docker"}
-  { import = "lazyvim.plugins.extras.lang.helm"}
-  { import = "lazyvim.plugins.extras.lang.git"}
-  { import = "lazyvim.plugins.extras.lang.rust"}
-  { import = "lazyvim.plugins.extras.lang.typescript"}
-  { import = "lazyvim.plugins.extras.lang.sql"}
-  { import = "lazyvim.plugins.extras.lang.terraform"}
+	{ import = "lazyvim.plugins.extras.lang.go" },
+	{ import = "lazyvim.plugins.extras.lang.python" },
+	{ import = "lazyvim.plugins.extras.lang.docker" },
+	{ import = "lazyvim.plugins.extras.lang.helm" },
+	{ import = "lazyvim.plugins.extras.lang.git" },
+	{ import = "lazyvim.plugins.extras.lang.rust" },
+	{ import = "lazyvim.plugins.extras.lang.typescript" },
+	{ import = "lazyvim.plugins.extras.lang.sql" },
+	{ import = "lazyvim.plugins.extras.lang.terraform" },
 
-  -- util
-  { import = "lazyvim.plugins.extras.util.chezmoi"}
+	-- util
+	{ import = "lazyvim.plugins.extras.util.chezmoi" },
 }
