648749b4218deee365854bca20739dd303743b41
Philip Krück <hi@philipkrueck.com>
2025-09-25 16:34:34 +0100

    feat: add nvim keymaps.lua

---
+++ dot_config/nvim/lua/config/keymaps.lua
@@ -0,0 +1,4 @@
+-- these keep the cursor in the middle when scrolling with ctrl d and u
+-- from https://github.com/ThePrimeagen/init.lua
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
