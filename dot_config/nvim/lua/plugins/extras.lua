8a8d9dd6d6e2ad33e37e22f66fba090b772120b2
Philip Krück <hi@philipkrueck.com>
2025-09-30 22:08:01 +0100

    feat: add nushell to lazyextras

--- dot_config/nvim/lua/plugins/extras.lua
+++ dot_config/nvim/lua/plugins/extras.lua
@@ -10,6 +10,7 @@ return {
 	{ import = "lazyvim.plugins.extras.lang.git" },
 	{ import = "lazyvim.plugins.extras.lang.rust" },
 	{ import = "lazyvim.plugins.extras.lang.typescript" },
+	{ import = "lazyvim.plugins.extras.lang.nushell" },
 	{ import = "lazyvim.plugins.extras.lang.sql" },
 	{ import = "lazyvim.plugins.extras.lang.terraform" },
 
